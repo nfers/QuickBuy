c09cbd963b100d745c18a0327b17eadfbdafaee4
Nay Ferreira <ncy.fers@gmail.com>
2019-12-16 21:57:23 -0300

    20191216#05: criando regras de validacao para classe de entidade

--- QuickBuy.Dominio/Entities/Entity.cs
+++ QuickBuy.Dominio/Entities/Entity.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Domain.Entities
 {
     public abstract class Entity
     {
+        public List<string> _messages { get; set; }
+        public List<string> ValidationMessages
+        {
+            get { return _messages ?? (_messages = new List<string>()); }
+        }
+
+        public abstract void Validate();
+        protected bool IsValid {
+            get { return !ValidationMessages.Any(); }
+        }
 
 
     }
