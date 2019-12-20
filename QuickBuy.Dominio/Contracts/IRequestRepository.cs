568374af6ac2f3669d9d5d868907a1d53e0d2297
Nayara Ferreira <nayara.ferreira@tron.com.br>
2019-12-20 08:52:50 -0300

    20191220#04: implementando repositorios das entidades

---
+++ QuickBuy.Dominio/Contracts/IRequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using QuickBuy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.Contracts
+{
+    interface IRequestRepository : IRepositoryBase<Request>
+    {
+
+    }
+}
---
+++ QuickBuy.Dominio/Contracts/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuickBuy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.Contracts
+{
+    interface IUserRepository : IRepositoryBase<User>
+    {
+        
+    }
+}
+
+//Interface de repositorio do usuario
