13c0c72c4c3652d8db1cf528ecf032ef19af8d30
Nayara Ferreira <nayara.ferreira@tron.com.br>
2019-12-20 10:05:32 -0300

    20191220#08: criacao do repositorio de usuario e repositorio base

---
+++ QuickBuy.Repositorio/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using QuickBuy.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repository.Repositories
+{
+    public class BaseRepository<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+        public BaseRepository()
+        {
+
+        }
+
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Updated(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QuickBuy.Repositorio/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuickBuy.Domain.Entities;
+using QuickBuy.Domain.Contracts;
+
+namespace QuickBuy.Repository.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IRepositoryUser
+    {
+        public UserRepository()
+        {
+         
+        }
+
+    }
+}
