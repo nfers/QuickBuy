5a64e760b185d022846e6fb60a34cb46e36919f5
Nay Ferreira <ncy.fers@gmail.com>
2019-12-22 20:12:02 -0300

    20191222#01: adicionando regras de validacao na entidade de produtos
    Signed-off-by: Nay Ferreira <ncy.fers@gmail.com>

--- QuickBuy.Dominio/Entities/Product.cs
+++ QuickBuy.Dominio/Entities/Product.cs
@@ -5,18 +5,21 @@ using System.Linq;
 
 namespace QuickBuy.Domain.Entities
 {
-    class Product : Entity
+    public class Product : Entity
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        
         public override void Validate()
         {
             ClearMessages();
 
             if (string.IsNullOrEmpty(Name))
-                AddCriti("Atention: name cannot be empty");
+                AddCriti("Importante - Produto: Nome do produto não pode ser vazio");
+            if (string.IsNullOrEmpty(Description))
+                AddCriti("Importante - Produto: Descrição do produto não pode ser vazio");
         }
     }
 }
