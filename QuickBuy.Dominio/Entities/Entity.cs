c57cb9e49d97d2e3965c70d318404c5c698d1d09
Nay Ferreira <ncy.fers@gmail.com>
2019-12-26 18:10:57 -0300

    20191226#02: ajuste no retorno das mensagens de erro

--- QuickBuy.Dominio/Entities/Entity.cs
+++ QuickBuy.Dominio/Entities/Entity.cs
@@ -7,7 +7,7 @@ namespace QuickBuy.Domain.Entities
 {
     public abstract class Entity
     {
-        public List<string> _messages { get; set; }
+        private List<string> _messages { get; set; }
         private List<string> ValidationMessages
         {
             get { return _messages ?? (_messages = new List<string>()); }
