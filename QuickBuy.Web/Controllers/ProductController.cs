9ed41f1a10c37bbb8914df9376d25c402e970c1f
Nay Ferreira <ncy.fers@gmail.com>
2020-01-05 22:06:53 -0300

    20200105#03: adicionando controller de produto

---
+++ QuickBuy.Web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IRepositoryProduct _productRepository;
+
+        public ProductController(IRepositoryProduct productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
