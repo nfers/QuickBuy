8f3dccf61a00ff12cead8b07321a1bb14dae9e03
Nay Ferreira <ncy.fers@gmail.com>
2020-01-24 22:55:17 -0300

    20200124#12: inclusao de estilizacao do componente de login e design da tela

--- QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: "app-login",
-    templateUrl: './login.component.html'
-    // styleUrls: ['./././././style.css']
+    templateUrl: './login.component.html',
+     styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent {
