585741d40134b75dfcf4e55db1c53df559db6046
Nay Ferreira <ncy.fers@gmail.com>
2020-01-24 23:05:28 -0300

    20200125#1: adicionando imagem a tela de login e adição de funções de click

--- QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
+    public email = 'ncy.fers@gmail.com';
+    entrar() : void {
+     alert('entrar no sistema')   ;
+    }
 
 };
