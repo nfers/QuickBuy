cc42c5493c75e07fe07c3b53daf412692190ea33
Nay Ferreira <ncy.fers@gmail.com>
2020-01-24 21:26:47 -0300

    20200124#08: criado controller para o usuario com metodo de get e post

---
+++ QuickBuy.Web/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Domain.Contracts;
+using QuickBuy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class UserController : Controller
+    {
+        private readonly IRepositoryUser _userRepository;
+
+        public UserController(IRepositoryUser userRepository)
+        {
+            _userRepository = userRepository;
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_userRepository.FindAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]User usuario)
+        {
+
+            try
+            {
+                _userRepository.Add(usuario);
+
+                return Created("api/user", usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+    }
+}
