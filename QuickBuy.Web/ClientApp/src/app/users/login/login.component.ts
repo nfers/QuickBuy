7c1842bea882989a515948720d2c947668f1f2f6
Nayara Ferreira <nayara.ferreira@tron.com.br>
2020-01-28 18:54:15 -0300

    20200128#03: incluindo validacao dos campos de login

--- QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
@@ -1,4 +1,3 @@
-import { AppComponent } from './../../app.component';
 import { Component } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 
@@ -12,18 +11,18 @@ export class LoginComponent {
     public user;
     usuarioAutenticado: boolean;
 
-    public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
 
     constructor() {
         this.user = new User();
     };
 
     entrar() {
-        if (this.user.email == "ncy.fers@gmail.com" && this.user.password == "123456") {
-            this.usuarioAutenticado = true;
-        }
-        else (this.usuarioAutenticado = false);
-     alert(this.usuarioAutenticado.valueOf());
+      console.log(this.user.email)
+    //     if (this.user.email == "ncy.fers@gmail.com" && this.user.password == "123456") {
+    //         this.usuarioAutenticado = true;
+    //     }
+    //     else (this.usuarioAutenticado = false);
+    //  alert(this.usuarioAutenticado.valueOf());
     };
 
 };
