5e8cb27e4155ea55866b4f301b5bda4bb5fbccc6
Nayara Ferreira <nayara.ferreira@tron.com.br>
2020-01-28 08:38:23 -0300

    20200128#02: incluindo validacao para persisitir usuarios

--- QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/users/login/login.component.ts
@@ -12,16 +12,18 @@ export class LoginComponent {
     public user;
     usuarioAutenticado: boolean;
 
+    public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
+
     constructor() {
         this.user = new User();
     };
-       
+
     entrar() {
         if (this.user.email == "ncy.fers@gmail.com" && this.user.password == "123456") {
             this.usuarioAutenticado = true;
         }
         else (this.usuarioAutenticado = false);
-     alert(this.usuarioAutenticado.valueOf());    
-    };    
+     alert(this.usuarioAutenticado.valueOf());
+    };
 
 };
